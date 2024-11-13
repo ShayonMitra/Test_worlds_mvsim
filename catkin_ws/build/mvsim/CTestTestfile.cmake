bdb800a47346377e3606b3faf78b9d07dedbdddb
Swaminathan S K <swamisathya2004@gmail.com>
2024-11-13 22:55:13 +0530

    removed build and devel

--- catkin_ws/build/mvsim/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/swaminathan/catkin_ws/src/mvsim
-# Build directory: /home/swaminathan/catkin_ws/build/mvsim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("modules")
-subdirs("mvsim-cli")
-subdirs("tests")
--- catkin_ws/build/mvsim/modules/msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/swaminathan/catkin_ws/src/mvsim/modules/msgs
-# Build directory: /home/swaminathan/catkin_ws/build/mvsim/modules/msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
